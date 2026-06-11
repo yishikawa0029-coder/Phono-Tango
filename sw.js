cf23fadc0023af9966720a39439283c2bf1d1966
yishikawa0029-coder <y.ishikawa0029@gmail.com>
2026-06-11 20:50:49 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Phono-Tango Service Worker
+const CACHE = 'phono-tango-v3';
+const ASSETS = ['./','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then((c) => Promise.allSettled(ASSETS.map((a) => c.add(a))))
+      .then(() => self.skipWaiting())
+  );
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((hit) =>
+      hit || fetch(e.request).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match('./'))
+    )
+  );
+});
